be6ed75b56d04f17860e46d790305a230087d2a3
dgallagher-ire <mobile2158857@gmail.com>
2016-12-08 23:14:58 +0000

    sample files and file reader

---
+++ src/jackson/demo/GetJSON.java
@@ -0,0 +1,30 @@
+package jackson.demo;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+public class GetJSON {
+
+	public static final String SAMPLE1 = "sample1.json";
+	
+	public static final String getJson(final String fileName) {
+		try {
+			final InputStream inputStream = GetJSON.class.getResourceAsStream(fileName);
+			final ByteArrayOutputStream output= new ByteArrayOutputStream();
+			byte[] buffer = new byte[1024];
+			int length;
+			while ((length = inputStream.read(buffer)) != -1) {
+				output.write(buffer, 0, length);
+			}
+			return output.toString("UTF-8");
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return null;
+	}
+	
+	public static void main(final String[] args){
+		System.out.println(getJson(SAMPLE1));
+	}
+
+}
