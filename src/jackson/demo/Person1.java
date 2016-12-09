f5d80cd328627e9b383d20b782b569eebe87cd0d
dgallagher-ire <mobile2158857@gmail.com>
2016-12-09 16:45:45 +0000

    json to java class

---
+++ src/jackson/demo/Person1.java
@@ -0,0 +1,96 @@
+package jackson.demo;
+
+public class Person1 {
+
+	private Name name;
+	private Gender gender;
+	private boolean verified;
+	private String userImage;
+	
+	public Person1(){
+	}
+	
+	@Override
+	public final String toString(){
+		final StringBuilder sb = new StringBuilder();
+		sb.append("First:").append(this.getName().getFirst()).append("\n");
+		sb.append("Last:").append(this.getName().getLast()).append("\n");
+		sb.append("Gender:").append(this.getGender().name()).append("\n");
+		sb.append("Verified:").append(this.isVerified()).append("\n");
+		sb.append("UserImage:").append(this.getUserImage()).append("\n");
+		return sb.toString();
+	}
+	
+	public final Name getName() {
+		return name;
+	}
+
+	public final void setName(Name name) {
+		this.name = name;
+	}
+
+	public final Gender getGender() {
+		return gender;
+	}
+
+	public final void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public final boolean isVerified() {
+		return verified;
+	}
+
+	public final void setVerified(boolean verified) {
+		this.verified = verified;
+	}
+
+	public final String getUserImage() {
+		return userImage;
+	}
+
+	public final void setUserImage(String userImage) {
+		this.userImage = userImage;
+	}
+
+	
+	public enum Gender {
+		MALE, FEMALE;
+	}
+	
+	public static final class Name {
+		
+		private String first;
+		private String last;
+		
+		public Name(){
+		}
+
+		public final String getFirst() {
+			return first;
+		}
+
+		public final void setFirst(String first) {
+			this.first = first;
+		}
+
+		public final String getLast() {
+			return last;
+		}
+
+		public final void setLast(String last) {
+			this.last = last;
+		}
+		
+	}
+	
+	/*
+	"name": {
+	"first": "Joe",
+	"last": "Sixpack"
+},
+"gender": "MALE",
+"verified": false,
+"userImage": "Rm9vYmFyIQ=="
+*/
+}
